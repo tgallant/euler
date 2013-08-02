39c35be809fcabe76537327d84b9799462476594
Tim Gallant <timothybrettgallant@gmail.com>
2013-08-02 05:40:58 -0400

    modified

--- euler05.pl
+++ euler05.pl
@@ -1,4 +1,27 @@
 #!/usr/bin/env perl -w
-use strict;
+#use strict;
 
+my $result = 2;
 
+foreach (3..20){
+  $result = lcd($result,$_);
+}
+   
+print "$result\n";
+
+sub lcd {
+  ($m,$n) = @_;
+  $gcd = gcd($m,$n);
+  return $m*$n/$gcd;
+}
+    
+sub gcd {
+  my($a,$b) = @_;
+  return $a if $b == 0;
+  if($b > $a){
+    gcd($b,$a);
+  }else{
+    $a = $a % $b;
+    gcd($b,$a);
+  }
+}
