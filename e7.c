acbe65e169e5c77983f8206a65ac202c5860d939
Tim Gallant <timothybrettgallant@gmail.com>
2013-08-13 12:40:12 -0400

    project euler 7

---
+++ e7.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<math.h>
+
+int prime(int);
+
+main()
+{
+        int a=0,b=1,ans=0;
+
+        while(a<10001)
+        {
+                b++;
+
+                if (prime(b) == 1)
+                {
+                        a++;
+                        ans=b;
+                }
+        }
+        printf("%d\n",ans);
+}
+
+
+int prime(int x)
+{
+        if (x==1)
+                return 0;
+        else if (x<4)
+                return 1;
+        else if (x%2==0)
+                return 0;
+        else if (x<9)
+                return 1;
+        else if (x%3==0)
+                return 0;
+        else
+        {
+                int r = (int) sqrt(x);
+                int f = 5;
+
+                while (f<=r)
+                {
+                        if (x%f==0)
+                        {
+                                return 0;
+                                break;
+                        }
+                        if (x%(f+2)==0)
+                        {
+                                return 0;
+                                break;
+                        }
+                        f=f+6;
+                }
+                return 1;
+        }
+}
