707976dddf419f3388a0c68465d0cfda03e0cfb4
tgallant <timothybrettgallant@gmail.com>
2013-08-20 01:17:33 -0700

    Update euler01.py

--- euler01.py
+++ euler01.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 
-import time
-
 total = 0
-
-start = time.time()
 
 for x in range(1,1000):
     if x%3==0 or x%5==0:
         total+=x
 
-elapsed = time.time() - start
 print total
-print elapsed
